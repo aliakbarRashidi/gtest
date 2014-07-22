06a0835d07e63296a88c3c157c41d0b65159aec4
kalashnik <kalashnik@apriorit.com>
2014-07-22 14:50:34 +0300

    Build process was optimized.

--- build-vc10.bat
+++ build-vc10.bat
@@ -1,10 +1,7 @@
-rmdir msvc\gtest /q /s
-rmdir msvc\x64 /q /s
-
-"%VS100COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /build "Release|Win32"
-"%VS100COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /build "Debug|Win32"
-"%VS100COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /build "Release|x64"
-"%VS100COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /build "Debug|x64"
+"%VS100COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /rebuild "Release|Win32"
+"%VS100COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /rebuild "Debug|Win32"
+"%VS100COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /rebuild "Release|x64"
+"%VS100COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /rebuild "Debug|x64"
 
 call mvn -f ./pom/include/pom.xml deploy
 call mvn -f ./pom/x64-windows-static-mt-msvc10-debug/pom.xml deploy
