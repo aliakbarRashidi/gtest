d441948f5370ead4542d16026d06d74d47052dee
kalashnik <kalashnik@apriorit.com>
2014-07-24 13:39:13 +0300

    Build commands were moved to POM files.

--- build-vc10.bat
+++ build-vc10.bat
@@ -1,13 +1,4 @@
-"%VS100COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /rebuild "Release|Win32" || goto :EOF
 call mvn -f ./pom/x86-windows-static-mt-msvc10-release/pom.xml deploy || goto :EOF
-
-"%VS100COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /rebuild "Debug|Win32" || goto :EOF
 call mvn -f ./pom/x86-windows-static-mt-msvc10-debug/pom.xml deploy || goto :EOF
-
-"%VS100COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /rebuild "Release|x64" || goto :EOF
 call mvn -f ./pom/x64-windows-static-mt-msvc10-release/pom.xml deploy || goto :EOF
-
-"%VS100COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /rebuild "Debug|x64" || goto :EOF
 call mvn -f ./pom/x64-windows-static-mt-msvc10-debug/pom.xml deploy || goto :EOF
-
-pause
--- build-vc11.bat
+++ build-vc11.bat
@@ -1,13 +1,4 @@
-"%VS110COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /rebuild "Release|Win32" || goto :EOF
 call mvn -f ./pom/x86-windows-static-mt-msvc11-release/pom.xml deploy || goto :EOF
-
-"%VS110COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /rebuild "Debug|Win32" || goto :EOF
 call mvn -f ./pom/x64-windows-static-mt-msvc11-debug/pom.xml deploy || goto :EOF
-
-"%VS110COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /rebuild "Release|x64" || goto :EOF
 call mvn -f ./pom/x64-windows-static-mt-msvc11-release/pom.xml deploy || goto :EOF
-
-"%VS110COMNTOOLS%..\IDE\devenv.com" msvc/gtest.sln /rebuild "Debug|x64" || goto :EOF
 call mvn -f ./pom/x86-windows-static-mt-msvc11-debug/pom.xml deploy || goto :EOF
-
-pause
